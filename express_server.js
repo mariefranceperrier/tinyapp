d7e887c798b40c8e9ac7b0f598745f64b5dbe87e
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-21 11:21:52 -0500

    new POST route for /login, that redirects browser back to /urls after successful login

--- express_server.js
+++ express_server.js
@@ -33,6 +33,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
