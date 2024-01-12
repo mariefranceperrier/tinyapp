9a6e3d11948a2f5a75425f431baf2c3fc6390494
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-01-12 08:27:04 -0500

    initial commit

---
+++ userData.js
@@ -0,0 +1,23 @@
+export const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+export const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "1a2b3c",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "1a2b3c",
+  }
+};
